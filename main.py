ad79c9b1c710d67814ac9f36a841c2cd1a6e6dec
Oleksandr Krasichkov <alexkrasichkov@gmail.com>
2026-03-19 21:56:39 +0100

    fixed the bug, now finds the minimal distance between two cities

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import math
 def main():
     cities = []
     with open('cities10k.txt','r') as file:
-        for line in islice(file, 10):
+        for line in islice(file, 1000):
             city, lat, lon = line.rstrip().split()
             cities.append((city, float(lat), float(lon)))
-    minv=math.sqrt(math.pow((cities[0][1] - cities[1][1]), 2) - math.pow((cities[0][1] - cities[1][1]), 2))  
+    minv=math.sqrt(math.pow((cities[0][1] - cities[1][1]), 2) - math.pow((cities[0][2] - cities[1][2]), 2))  
     mincity1 = cities[0][0]
     mincity2 =cities[1][0]
     mincoord1 = [cities[0][1], cities[0][2]]
@@ -22,10 +22,10 @@ def main():
             distance=math.sqrt(lats*lats + lons*lons)
             if(minv > distance):
                 minv = distance
-                mincity1 = cities[city][0]
-                mincity2=cities[city2][0]
-                mincoord1=[cities[city][1], cities[city][2]]
-                mincoord2 = [cities2[1], cities[city2][2]]
+                mincity1 = city[0]
+                mincity2=city2[0]
+                mincoord1=[city[1], city[2]]
+                mincoord2 = [city2[1], city2[2]]
     print("The shortest-distanced cities are:")
     print(mincity1, mincoord1, mincity2, mincoord2, "with distance: ", minv)
 
