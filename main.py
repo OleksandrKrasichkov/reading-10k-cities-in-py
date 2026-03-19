06eaa5fdf02d7e01e948970fd5687dce63d6371d
Oleksandr Krasichkov <alexkrasichkov@gmail.com>
2026-03-19 20:35:51 +0100

    printed out first 10 cities

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from itertools import islice
 def main():
     with open('cities10k.txt','r') as file:
-        for line in file:
-            pass
+        for line in islice(file, 10):
+            city, lat, lon = line.rstrip().split()
+            print(city, lat, lon)
+
 
 if __name__ == '__main__':
     main()
