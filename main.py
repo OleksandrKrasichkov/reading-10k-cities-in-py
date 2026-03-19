37fd47cfa4c700f139271f003deaa5cf2dff7aa7
Oleksandr Krasichkov <alexkrasichkov@gmail.com>
2026-03-19 20:37:34 +0100

    just checking git

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     with open('cities10k.txt','r') as file:
         for line in islice(file, 10):
             city, lat, lon = line.rstrip().split()
-            print(city, lat, lon)
+            print(city, lat, lon, 1)
 
 
 if __name__ == '__main__':
